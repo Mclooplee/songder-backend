22ab4e372c89e89a63c1b4f06b1b79e7e621c41b
Mc Looplee <mclooplee@mac.home>
2026-05-17 17:25:42 +0200

    fix dotenv for production

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: false });
+const dotenv = require('dotenv');
+if (process.env.NODE_ENV !== 'production') dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
